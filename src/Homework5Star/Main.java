cc596a99b2117b007ee1f87ccaa81c4a5a3e2007
Li2nga <137508531+Li2nga@users.noreply.github.com>
2024-06-09 19:17:46 +0300

    1.00 Task1 (switch)

--- src/Homework5Star/Main.java
+++ src/Homework5Star/Main.java
@@ -1,7 +1,32 @@
 package Homework5Star;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        task1();
+    }
 
+    public static void task1() {
+        System.out.println("Задача 1");
+        try {
+            method1(13);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+        method1(12);
+        method1(4);
+        method1(7);
+        method1(10);
+    }
+
+    public static void method1(int monthNumber) {
+        String season = switch (monthNumber) {
+            case 12, 1, 2 -> "Зима";
+            case 3, 4, 5 -> "Весна";
+            case 6, 7, 8 -> "Лето";
+            case 9, 10, 11 -> "Осень";
+            default -> throw new RuntimeException("Некорректный номер месяца");
+        };
+        System.out.println(season);
     }
 }
+
