4f2657297b3b6add98704f96ea584336c56750c0
Li2nga <137508531+Li2nga@users.noreply.github.com>
2024-06-25 22:00:59 +0300

    1.02 Task1 (Book)

--- src/Homework12/Main.java
+++ src/Homework12/Main.java
@@ -8,12 +8,15 @@ public class Main {
 
         System.out.println("a1.getName() = " + a1.getName());
         System.out.println("a2.getName() = " + a2.getName());
+        System.out.println("a1.getSurname() = " + a1.getSurname());
+        System.out.println("a2.getSurname() = " + a2.getSurname());
 
         Book b1 = new Book("Название1", a1, 1911);
         Book b2 = new Book("Название2", a2, 1956);
 
         System.out.println("b1.getTitle() = " + b1.getTitle());
         System.out.println("b1.getAuthor() = " + b1.getAuthor());
+
         System.out.println("b1.getPublishingYear() = " + b1.getPublishingYear());
         b1.setPublishingYear(1800);
         System.out.println("b1.getPublishingYear() = " + b1.getPublishingYear());
