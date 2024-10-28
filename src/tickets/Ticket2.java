f30244ce15edc713c96fdca7c5fbd7c393dc9938
Li2nga <137508531+Li2nga@users.noreply.github.com>
2024-10-28 14:46:30 +0300

    1.2 Ticket 1-6

--- src/tickets/Ticket2.java
+++ src/tickets/Ticket2.java
@@ -2,7 +2,6 @@ package tickets;
 
 public class Ticket2 {
     public static Boolean checkPalindrome(String myString) {
-
         String reversed = new StringBuilder(myString).reverse().toString();
         return myString.equalsIgnoreCase(reversed);
     }
--- src/tickets/Ticket4.java
+++ src/tickets/Ticket4.java
@@ -1,24 +1,23 @@
 package tickets;
 
 public class Ticket4 {
-    public static String deleteDuplicatesAndConcat(String[] stringsArr) {
-        String[] outArr = new String[stringsArr.length];
+    public static String deleteDuplicatesAndConcat(String[] stringArr) {
+        String[] outArr = new String[stringArr.length];
         int lastIndex = 0;
-        for (String s : stringsArr) {
+        for (String s : stringArr) {
             for (int i = 0; ; i++) {
                 if (outArr[i] == null) {
                     outArr[i] = s;
                     lastIndex = i;
                     break;
                 }
-                if (outArr[i].equalsIgnoreCase(s)) {
+                if (outArr[i].equals(s)) {
                     break;
                 }
             }
         }
         String[] finalArr = new String[lastIndex + 1];
         System.arraycopy(outArr, 0, finalArr, 0, lastIndex + 1);
-
         return String.join(", ", finalArr);
     }
 }
