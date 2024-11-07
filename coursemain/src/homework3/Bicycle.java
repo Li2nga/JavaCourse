75b5c337ec873e53329aea23d7707baec5ad27a2
Li2nga <137508531+Li2nga@users.noreply.github.com>
2024-11-07 22:53:02 +0300

    Task 2.3

---
+++ coursemain/src/homework3/Bicycle.java
@@ -0,0 +1,11 @@
+package homework3;
+
+public class Bicycle {
+
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
