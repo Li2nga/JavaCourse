5a4e8e309af89d5b50f6b87a2903a94c0e2f5448
Li2nga <137508531+Li2nga@users.noreply.github.com>
2024-11-20 18:36:18 +0300

    1.2 Calculator

--- coursemain/src/course2/homework4/src/main/java/calculator/controller/CalculatorController.java
+++ coursemain/src/course2/homework4/src/main/java/calculator/controller/CalculatorController.java
@@ -13,22 +13,22 @@ public class CalculatorController {
         this.calculatorService = calculatorService;
     }
 
-    @GetMapping(path = "/calculator/plus?num1=5&num2=5")
+    @GetMapping(path = "/calculator/plus?num1&num2")
     public String plus(@RequestParam int num1, @RequestParam int num2) {
         return calculatorService.plus(num1, num2);
     }
 
-    @GetMapping(path = "/calculator/minus?num1=5&num2=5")
+    @GetMapping(path = "/calculator/minus?num1&num2")
     public String minus(@RequestParam int num1, @RequestParam int num2) {
         return calculatorService.minus(num1, num2);
     }
 
-    @GetMapping(path = "/calculator/multiply?num1=5&num2=5")
+    @GetMapping(path = "/calculator/multiply?num1&num2")
     public String multiply(@RequestParam int num1, @RequestParam int num2) {
         return calculatorService.multiply(num1, num2);
     }
 
-    @GetMapping(path = "/calculator/divide?num1=5&num2=5")
+    @GetMapping(path = "/calculator/divide?num1&num2")
     public String divide(@RequestParam int num1, @RequestParam int num2) {
         return calculatorService.divide(num1, num2);
     }
--- coursemain/src/course2/homework4/src/main/java/calculator/service/CalculatorServiceImpl.java
+++ coursemain/src/course2/homework4/src/main/java/calculator/service/CalculatorServiceImpl.java
@@ -6,21 +6,25 @@ import org.springframework.stereotype.Service;
 public class CalculatorServiceImpl implements CalculatorService {
     @Override
     public String plus(int num1, int num2) {
-        return "5 + 5 = 10";
+        return num1 + "+" + num2 + "=" + (num1 + num2);
     }
 
     @Override
     public String minus(int num1, int num2) {
-        return "5 − 5 = 0";
+        return num1 + "-" + num2 + "=" + (num1 - num2);
     }
 
     @Override
     public String multiply(int num1, int num2) {
-        return "5 * 5 = 25";
+        return num1 + "*" + num2 + "=" + (num1 * num2);
     }
 
     @Override
     public String divide(int num1, int num2) {
-        return "5 / 5 = 1";
+        if (num2 == 0) {
+            return "Деление на ноль невозможно ";
+        } else {
+            return num1 + "/" + num2 + "=" + ((double) num1 / (double) num2);
+        }
     }
 }
